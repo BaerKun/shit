5182a26bcf8f8e3730aac3ecfc2e20850cd9c820
Bear Kun <bear_kun@outlook.com>
2025-09-28 22:36:57 +0800

    [add] lll::sub

--- lll/integer.cpp
+++ lll/integer.cpp
@@ -10,29 +10,35 @@ static inline void add64(const uint64_t a, const uint64_t b, uint64_t &carry,
   out = sum;
 }
 
+static inline void sub64(const uint64_t a, const uint64_t b, uint64_t &borrow,
+                         uint64_t &out) {
+  const uint64_t diff = a - b;
+  const uint64_t borrow_ = a < b || diff < borrow;
+  out = diff - borrow;
+  borrow = borrow_;
+}
+
 static int ucmpr(const std::vector<uint64_t> &a,
-const std::vector<uint64_t> &b) {
+                 const std::vector<uint64_t> &b) {
   const size_t size_a = a.size();
   const size_t size_b = b.size();
 
-  if (size_a > size_b)
-    return 1;
-  if(size_b > size_a)
-    return -1;
+  if (size_a > size_b) return 1;
+  if (size_b > size_a) return -1;
 
-  for(size_t i = size_a; i--;) {
-    if(a[i] > b[i]) return 1;
-    if(a[i] < b[i]) return -1;
+  for (size_t i = size_a; i--;) {
+    if (a[i] > b[i]) return 1;
+    if (a[i] < b[i]) return -1;
   }
   return 0;
 }
 
 int cmpr(const Integer &a, const Integer &b) {
-  if(a.neg_) {
-    if(b.neg_) return ucmpr(b.data_, a.data_);
+  if (a.neg_) {
+    if (b.neg_) return ucmpr(b.data_, a.data_);
     return -1;
   }
-  if(b.neg_) return 1;
+  if (b.neg_) return 1;
   return ucmpr(a.data_, b.data_);
 }
 
@@ -51,6 +57,7 @@ static void uadd(const std::vector<uint64_t> &max,
   for (size_t i = 0; i < size_min; i++) {
     add64(max[i], min[i], carry, out[i]);
   }
+
   for (size_t i = size_min; i < size_max; i++) {
     if (carry == 0) {
       memcpy(out.data() + i, max.data() + i, (size_max - i) * 8);
@@ -63,6 +70,29 @@ static void uadd(const std::vector<uint64_t> &max,
 
 static void usub(const std::vector<uint64_t> &max,
                  const std::vector<uint64_t> &min, std::vector<uint64_t> &out) {
+  const size_t size_max = max.size();
+  const size_t size_min = min.size();
+  uint64_t borrow = 0;
+
+  out.resize(size_max);
+  for (size_t i = 0; i < size_min; i++) {
+    sub64(max[i], min[i], borrow, out[i]);
+  }
+
+  if (size_max == size_min) {
+    for (size_t i = size_max; --i != 0;) {
+      if (out[i] == 0) out.pop_back();
+    }
+  } else {
+    for (size_t i = size_min; i < size_max; i++) {
+      if (borrow == 0) {
+        memcpy(out.data() + i, max.data() + i, (size_max - i) * 8);
+        return;
+      }
+      sub64(max[i], 0, borrow, out[i]);
+    }
+    if (out[size_max - 1] == 0) out.pop_back();
+  }
 }
 
 void add(const Integer &a, const Integer &b, Integer &out) {
@@ -70,16 +100,38 @@ void add(const Integer &a, const Integer &b, Integer &out) {
   const Integer &umax = ucmpr_res > 0 ? a : b;
   const Integer &umin = ucmpr_res > 0 ? b : a;
 
-  if(umax.neg_ == umin.neg_) {
+  if (umax.neg_ == umin.neg_) {
     uadd(umax.data_, umin.data_, out.data_);
   } else {
-    if(ucmpr_res == 0) {
-      out = Integer(0);
+    if (ucmpr_res == 0) {
+      out = 0;
       return;
     }
     usub(umax.data_, umin.data_, out.data_);
   }
   out.neg_ = umax.neg_;
+}
+
+void sub(const Integer &a, const Integer &b, Integer &out) {
+  const int ucmpr_res = ucmpr(a.data_, b.data_);
+
+  if (ucmpr_res == 0 && a.neg_ == b.neg_) {
+    out = 0;
+  } else if (ucmpr_res == 1) {
+    if (a.neg_ == b.neg_) {
+      usub(a.data_, b.data_, out.data_);
+    } else {
+      uadd(a.data_, b.data_, out.data_);
+    }
+    out.neg_ = a.neg_;
+  } else {
+    if (a.neg_ == b.neg_) {
+      usub(b.data_, a.data_, out.data_);
+    } else {
+      uadd(b.data_, a.data_, out.data_);
+    }
+    out.neg_ = !b.neg_;
+  }
 }
 
 } // namespace lll
